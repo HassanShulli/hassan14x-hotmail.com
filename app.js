587e44974b8fce0ccfaecb07039153452ec2f622
hassan.a <hassan.a@tractive.com.my>
2020-04-13 10:46:35 +0800

    code updated to get status code

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const got = require('got');
  
-const validUrl = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
-const inValidUrl = 'https://api.nasa.gov/planetary/apodinvalid';
+const validUrl = 'https://gorest.co.in/public-api/users?_format=json&access-token=2yXEckr1AWV1LTyZKgNwzQj9_xym5N33161Y';
+const inValidUrl = 'https://gorest.co.in/public-api/userss?_format=json&access-token=2yXEckr1AWV1LTyZKgNwzQj9_xym5N33161Y';
 
 getData(validUrl);
 getData(inValidUrl);
 
+getData(validUrl).then((res) => {
+	console.log(res);
+})
+
+getData(inValidUrl).then((res) => {
+	console.log(res);
+})
+
 function getData(requestUrl) {
-	got(requestUrl).then(response => {
-	  if (response) {
-	  	console.log('Response Code : 200');
-	  }
-	}).catch(error => {
-	  	console.log('Response Code : 500. The following url is invalid : ', requestUrl);
-	});
+	return new Promise(function(resolve, reject) {
+		got(requestUrl).then(response => {
+			let result = JSON.parse(response.body);
+			console.log('response status : ', result._meta.success);
+			if (result._meta.success) {
+				return resolve(result._meta.code)
+			} else {
+				return reject(new Error('statusCode=' + result._meta.code));
+			}
+		}).catch(error => {
+		return reject(error);
+	})
+	})
 }
